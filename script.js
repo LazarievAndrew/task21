ab7998cd42471f71e0979e2dfe95aeeef3239513
Andrew Lazariev <LazarievAndrew@users.noreply.github.com>
2019-08-04 02:19:38 +0300

    ver.1.1

--- script.js
+++ script.js
@@ -7,38 +7,28 @@
 // Нужно сгенерировать список из элементов, 
 // а если в массиве встречается массив то делать вложенный список.
 
-function innerArr (arr){
+function generateList (arr, parentNode){    // Чтобы сделать через рекурсию, добавил еще один аргумент
 
-    var fragment = document.createDocumentFragment();
-    var ul = document.createElement('ul');
-    var li = document.createElement('li');
-
-    for (var i = 0; i < arr.length; i++){
-        if (!isNaN(+arr[i])){
-            var cloneLi = li.cloneNode();
-            cloneLi.innerText = arr[i];
-            ul.appendChild(cloneLi);
-
-        } else {
-
-            var ol = document.createElement('ol');
-            var newLi = document.createElement('li');
-
-            for ( var j = 0; j < arr[i].length; j++){
-                var cloneNewLi = newLi.cloneNode();
-                cloneNewLi.innerText = arr[i][j];
-                ol.appendChild(cloneNewLi);
-            }
-
-            cloneLi.appendChild(ol);
-        }
-    }
+    var ul = document.createElement('ul'),
+        fragment = document.createDocumentFragment();
     
-    fragment.appendChild(ul);
-    var body = document.querySelector('body');
-    body.appendChild(fragment);
-}
+    arr.forEach(function(element){
 
-innerArr ([1,2, [1.1,1.2,1.3] ,3]);
+        var li = document.createElement('li');
 
-innerArr ([4,5, [1.1,1.2,1.3] ,6, [1.1,1.2,1.3]]);
+        fragment.appendChild(li);
+
+        Array.isArray(element) ? generateList (element, li) : li.innerText = element;
+
+        ul.appendChild(fragment);
+
+    });
+    
+    parentNode.insertBefore(ul,parentNode.children[0]);
+};
+
+var main = document.getElementById('main');
+
+generateList ([4,5,6,3], main);
+generateList ([4,5, [1.1,1.2,1.3], 6, [1.1,1.2,1.3]], main);
+generateList ([1,2, [1.1,1.2,1.3], 3], main);
